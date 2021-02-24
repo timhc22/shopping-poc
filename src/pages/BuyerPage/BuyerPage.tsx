9207101ee92f6f5b7312db69e933264de5bce94c
Timothy Coleman <timhc22@gmail.com>
2021-02-24 20:56:56 +0000

    Add back links

--- src/pages/BuyerPage/BuyerPage.tsx
+++ src/pages/BuyerPage/BuyerPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function BuyerPage(): JSX.Element {
 
   return (
     <div>
+      <Link to="/">
+        &larr; Back
+      </Link>
+
       <h1>Buyer</h1>
       Test
     </div>
--- src/pages/SellerPage/SellerPage.tsx
+++ src/pages/SellerPage/SellerPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SellerPage(): JSX.Element {
 
   return (
     <div>
+      <Link to="/">
+        &larr; Back
+      </Link>
+
       <h1>Seller</h1>
       Test
     </div>
